b3bf1fa3c194f3ecb4182164b4aebc475b4fedc0
AlexKii <andallxr@gmail.com>
2023-02-08 18:05:22 +0200

    first

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.*;
+
+public class App 
+{
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    public static void main( String[] args )
+    {
+
+        Map<String, String> myName = new TreeMap<>(Comparator.reverseOrder());
+        myName.put("lastName", "Kiiashchenko");
+        myName.put("name", "Alex");
+        System.out.println(GSON.toJson(myName));
+
+    }
+}
